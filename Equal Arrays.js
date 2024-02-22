b045e5af780f1eb86b55c8f4c0b3c801e3105bff
Svetlin Garabedyan <146958421+SvetlinGG@users.noreply.github.com>
2024-02-22 12:00:45 +0100

    Add files via upload

---
+++ Equal Arrays.js
@@ -0,0 +1,32 @@
+function equal(arr1,arr2){
+    let sum1 = 0;
+    let sum2 = 0;
+    for ( let i =0; i < arr1.length; i++){
+        arr1[i] = Number(arr1[i]);
+        sum1 += arr1[i];
+    }
+    for ( let i =0; i <arr2.length; i++){
+        arr2[i] = Number(arr2[i]);
+        sum2 += arr2[i]; 
+    }
+
+    let isTrue = true ;
+    for ( let i =0; i < arr1.length; i++){
+    
+        if ( arr1[i] !== arr2[i]){
+            console.log(`Arrays are not identical. Found difference at ${i} index`);
+            isTrue = false;
+            break;
+        }else if ( sum1 ===sum2 ){
+            console.log(`Arrays are identical. Sum: ${sum2}`);
+        }
+    
+    }
+    
+    
+}
+equal(['10','20','30'], 
+      ['10','20','30']);
+equal(['1','2','3','4','5'], 
+      ['1','2','4','4','5']);
+equal(['1'], ['10'])
